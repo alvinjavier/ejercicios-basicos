ad366f7be521ff685ff1fd425642a09b4456c054
alvinjavier <103376929+alvinjavier@users.noreply.github.com>
2022-04-18 22:26:13 -0400

    Add files via upload

---
+++ ejercicio8.js
@@ -0,0 +1,18 @@
+const prompt = require('prompt');
+prompt.start();
+prompt.get ( ['valor1'], function (err, result) {
+     valor1 = parseInt (result.valor1);
+     valor2 = 2;
+    while 
+    (valor1 % valor2 !== 0 && valor2 < valor1 / 2 ) {
+        valor2++;
+        }
+        if (valor1 % valor2 !== 0) {
+            console.log ("El número es primo");
+            } 
+            else {
+            console.log ("El número no es primo");
+            }
+
+
+});
